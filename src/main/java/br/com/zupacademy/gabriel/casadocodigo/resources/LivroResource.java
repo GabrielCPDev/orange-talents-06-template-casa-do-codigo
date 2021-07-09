986b9201ae2c3ec884a3ca19805c23c06cb6467a
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-07-09 19:37:25 -0300

    Adicionando método que lista livros

--- src/main/java/br/com/zupacademy/gabriel/casadocodigo/resources/LivroResource.java
+++ src/main/java/br/com/zupacademy/gabriel/casadocodigo/resources/LivroResource.java
@@ -1,15 +1,21 @@
 package br.com.zupacademy.gabriel.casadocodigo.resources;
 
 import java.net.URI;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -34,5 +40,12 @@ public class LivroResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(){		
+		List<Livro> list = livroRepository.findAll();
+		List<LivroDTO> listDto = list.stream().map(x -> new LivroDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
